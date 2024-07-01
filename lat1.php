05b06149cb2e2e2bc29b420107c2bc2c8a3cfecd
Elfin-afk <elfinbahrufawaid@gmail.com>
2024-07-01 12:02:03 +0700

    ini file pertama

---
+++ lat1.php
@@ -0,0 +1,12 @@
+<?php
+echo "hello world!!!!":
+//perintah untuk add atau tambah
+//1. git add . atau git add nama_folder/file
+//perintah untuk commit atau komentar
+//2. git commit -m "komentarnya apa"
+//perintah untuk upload atau push
+//3. git push -u origin master
+//pus adalah perintah, origin adalah nama remote
+//master adalah nama branch
+
+?>
